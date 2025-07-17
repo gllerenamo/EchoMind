ea398fada578ef3a4a9899e2e9a3a08b8923c355
gllerenamo <gllerenamo@unsa.edu.pe>
2025-07-17 15:17:01 -0500

    fix: Conexión a Supabase

--- apps/server/src/config/database.config.ts
+++ apps/server/src/config/database.config.ts
@@ -5,7 +5,7 @@ import { User, Patient, Doctor, ClinicalCase, Consultation, Message } from '../e
 export const getDatabaseConfig = (configService: ConfigService): TypeOrmModuleOptions => ({
   type: 'postgres',
   host: configService.get('DB_HOST'),
-  port: configService.get('DB_PORT', 5432),
+  port: parseInt(configService.get('DB_PORT') || '5432', 10),
   username: configService.get('DB_USERNAME'),
   password: configService.get('DB_PASSWORD'),
   database: configService.get('DB_DATABASE'),
